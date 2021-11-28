bc3ac4749f75727601f85825d5c0046e257c4e80
Max <sonthaya.nt@hotmail.com>
2021-11-29 01:09:12 +0700

    Fixed read temp fail

--- ports/esp32/boards/openbit/modules/sensor.py
+++ ports/esp32/boards/openbit/modules/sensor.py
@@ -2,13 +2,15 @@
 
 from machine import Pin, I2C, ADC
 
-LM73_ADDR = const(0x4D)
+TMP75_ADDR = const(0x48)
+
+i2c1 = I2C(1, scl=Pin(21), sda=Pin(22), freq=100000)
+i2c1.writeto_mem(TMP75_ADDR, 0x01, '\x00')
 
 def temperature():
-    i2c1 = I2C(1, scl=Pin(21), sda=Pin(22), freq=100000)
-    data = i2c1.readfrom_mem(LM73_ADDR, 0x00, 2)
-    temp = (((data[0] & 0x7F) << 8)| data[1]) >> 5
-    temp = temp * 0.25
+    data = i2c1.readfrom_mem(TMP75_ADDR, 0x00, 2)
+    temp = ((data[0] << 8) | data[1]) >> 4
+    temp = temp * 0.0625
     temp = temp * (-1 if (data[0] & 0x80) != 0 else 1)
     return temp
 
