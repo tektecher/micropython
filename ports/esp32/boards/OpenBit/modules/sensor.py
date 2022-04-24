19e69dffa777a99a9133f42c80daa49ac6a137ae
Max <sonthaya.nt@hotmail.com>
2022-04-25 03:57:15 +0700

    Add sersor.light() support with BH1750
    Update for OpenBIT V2.1, Add BH1750 sensor

--- ports/esp32/boards/OpenBit/modules/sensor.py
+++ ports/esp32/boards/OpenBit/modules/sensor.py
@@ -1,21 +1,37 @@
 # Dev by Sonthaya Nongnuch
 
 from machine import Pin, I2C, ADC
+from time import sleep
 
 TMP75_ADDR = const(0x4C)
+BH1750_ADDR = const(0x23)
 
 i2c1 = I2C(1, scl=Pin(21), sda=Pin(22), freq=100000)
 i2c1.writeto_mem(TMP75_ADDR, 0x01, '\x00')
 
 def temperature():
-    data = i2c1.readfrom_mem(TMP75_ADDR, 0x00, 2)
-    temp = ((data[0] << 8) | data[1]) >> 4
-    temp = temp * 0.0625
-    temp = temp * (-1 if (data[0] & 0x80) != 0 else 1)
-    return temp
+    try:
+        data = i2c1.readfrom_mem(TMP75_ADDR, 0x00, 2)
+        temp = ((data[0] << 8) | data[1]) >> 4
+        temp = temp * 0.0625
+        temp = temp * (-1 if (data[0] & 0x80) != 0 else 1)
+        return temp
+    except:
+        return -99
+    return -98
 
 def microphone():
     adc = ADC(Pin(39))
     adc.atten(ADC.ATTN_11DB)
     adc.width(ADC.WIDTH_12BIT)
     return adc.read()
+
+i2c1.writeto(BH1750_ADDR, b'\x10')
+
+def light():
+    try:
+        h, l = i2c1.readfrom(BH1750_ADDR, 2)
+        return int(((h << 8) | l) / 1.2)
+    except:
+        return -99
+    return -98
